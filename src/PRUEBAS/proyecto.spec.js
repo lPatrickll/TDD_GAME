8db764a5393a7bfc1b9eb1e681dec2db866715cb
Arturo <arturo.veliz@ucb.edu.bo>
2024-05-29 00:51:02 -0400

    Se movio pruebas de objeto Proyecto de 5ta HU a proyecto.spec.js

--- src/PRUEBAS/proyecto.spec.js
+++ src/PRUEBAS/proyecto.spec.js
@@ -32,5 +32,50 @@ describe("Proyecto", () => {
       { cantPruebas: 4, cantLineas: 4, cobertura: 4 }, { cantPruebas: 10, cantLineas: 9, cobertura: 10}];
     expect(proyecto.mostrarCommits()).toEqual([]);
   });
+
+
+
+  // ********************************************************************
+  // 5ta HU Arturo
+
+    it("El proyecto deberia de devolver el puntaje de 0", () => {
+
+        const proyecto = new Proyecto("Proyecto1");
+        expect(proyecto.getPuntajePruebas()).toEqual(0);
+    });
+
+    it("El proyecto deberia de devolver el puntaje de 50% para la mitad de pruebas aprobadas", () => {
+
+        const proyecto = new Proyecto("Proyecto2");
+        proyecto.aniadirCommitFinal(2, 4, 4,1);
+
+        expect(proyecto.getPuntajePruebas()).toEqual(50);
+    });
+
+
+
+    it("El proyecto deberia de mostrar commits con pruebas aprobadas", () => {
+        const proyecto = new Proyecto("Proyecto2");
+        proyecto.aniadirCommitFinal(2, 2, 30,2);
+        let expectedArray = [{
+        cantPruebas: 2,
+        cantPruebasAprob:2,
+        cantLineas: 2,
+        cobertura: 30,
+        recomendacion: "Buen trabajo en las pruebas aprobadas. La cobertura de código es baja, considera añadir más pruebas."
+        }];
+        expect(proyecto.mostrarCommitCompleto()).toEqual(expectedArray);
+    });
+
+    it("El proyecto deberia de devolver el puntaje de 100% todas las pruebas aprobadas", () => {
+
+        const proyecto = new Proyecto("Proyecto2");
+        proyecto.aniadirCommitFinal(2, 4, 4,2);
+        proyecto.aniadirCommitFinal(2, 4, 4,2);
+        proyecto.aniadirCommitFinal(2, 4, 4,2);
+        proyecto.aniadirCommitFinal(2, 4, 4,2);
+
+        expect(proyecto.getPuntajePruebas()).toEqual(100);
+    });
  
 });
